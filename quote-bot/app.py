ae56a9b51fd079b60aa5d0b3716418d665dbad3d
Kyle Speight <kyle@discord.kmg.work.gd>
2025-01-27 23:24:51 -0800

    Create app.py

---
+++ quote-bot/app.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def quote(ctx):
+    # Fetch a quote from the API
+    response = requests.get("https://zenquotes.io/api/random")
+    
+    if response.status_code == 200:
+        data = response.json()
+        quote = data[0]['q']
+        author = data[0]['a']
+        await ctx.send(f'"{quote}" - {author}')
+    else:
+        await ctx.send("Sorry, I couldn't fetch a quote at the moment.")
+
+bot.run('YOUR_BOT_TOKEN')
